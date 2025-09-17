94cd0b34ac7e6487a2bb06c385253cda55e98b8f
howardmarqq <howardmarq75@gmail.com>
2025-09-17 09:21:41 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,14 @@
+//Define Variables - Line 1
+let myName = "Marq";
+let title = "Student"
+let education = "Oral Rovers University";
+let experience = "Tutoring, Chick Fil A, Software";
+let skills = ["Running, Coding, Singing"]
+
+
+//Connect Variables to the HTML
+document.getElementBYId("my-name").textcontent = myName;
+document.getElementBYId("title").textcontent = title;
+document.getElementById("Education").textcontent = education
+document.getElementBYId("Experience").textcontent = experience;
+document.getElementBYId("Skills").textcontent = skills;
